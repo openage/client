9dc420dd5d0c1e89018767bb79590ac31e31bb39
mohitsharma135 <mohit.sharma1@yatra.com>
2022-07-27 14:39:16 +0530

    lookups service in connect client

--- services/connect.js
+++ services/connect.js
@@ -2,3 +2,4 @@ const crud = require('../helpers/crud')
 
 exports.integrations = crud('connect', 'integrations')
 exports.providers = crud('connect', 'providers')
+exports.lookups = crud('connect', 'lookups')
