dea3702688abf2fa0e69cb86be927c5618ac619a
Sunny Parkash <sunny.parkash@yatra.com>
2022-07-27 10:15:59 +0000

    Update services/connect.js

--- services/connect.js
+++ services/connect.js
@@ -2,4 +2,22 @@ const crud = require('../helpers/crud')
 
 exports.integrations = crud('connect', 'integrations')
 exports.providers = crud('connect', 'providers')
-exports.lookups = crud('connect', 'lookups')
+const lookups = crud('connect', 'lookups')
+
+// lookups('countries').find('in')
+exports.lookups = (area) => {
+    return {
+        find: async (code, context) => {
+            let page = await lookups.search({
+                area: area,
+                code: code
+            }, context)
+
+            if(!page.items || !page.items.length) {
+                return null
+            }
+
+            return page.items[0]
+        }
+    }
+}
